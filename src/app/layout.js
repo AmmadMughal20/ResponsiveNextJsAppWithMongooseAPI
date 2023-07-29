bda4c226f7ce3788d0b6ae0eb0269afa67ca94bd
Muhammad Ammad <ammadmughal567@gmail.com>
2023-07-29 23:33:15 +0500

    Corrected Mulish font error

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import './globals.css';
 import { Mulish } from 'next/font/google';
 
 const mulish = Mulish({
-  weight: ['200','300','400','500','600','700','800','900','1000'],
+  weight: ['300','400','500','600','700','800','900'],
   subsets: ['latin'],
   display: 'swap'
 })
